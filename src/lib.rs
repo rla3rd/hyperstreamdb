2bc2a4e8a8b626b9ca1121dde989fad32ede8eb3
rla3rd <rla3rd@gmail.com>
2026-09-09 19:55:03 -0400

    Fix cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -8,7 +8,6 @@
     clippy::non_canonical_partial_ord_impl,
     clippy::needless_late_init,
     clippy::collapsible_match,
-
     unknown_lints
 )]
 // Copyright (c) 2026 Richard Albright. All rights reserved.
